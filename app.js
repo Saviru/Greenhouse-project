423cb56985dc08c9774ac241beecd3a150603573
Saviru Kashmira Atapattu <savirukashmira@gmail.com>
2024-10-10 00:34:56 +0530

    update firebase

--- app.js
+++ app.js
@@ -1,3 +1,36 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-app.js";
+import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/10.14.0/firebase-database.js"
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBxVSVunA5WdcuMaAxpQr-ySdNvUTz-yZQ",
+  authDomain: "fc-gh-db.firebaseapp.com",
+  databaseURL: "https://fc-gh-db-default-rtdb.firebaseio.com",
+  projectId: "fc-gh-db",
+  storageBucket: "fc-gh-db.appspot.com",
+  messagingSenderId: "434031960841",
+  appId: "1:434031960841:web:6ea9a8f0d77925ac63e9d8",
+  measurementId: "G-TZWZLY94R7"
+};
+
+// Initialize Firebase
+let data = [];
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+const dbRef = ref(db, "test");
+
+function fetchValue() {
+    onValue(dbRef, function(snapshot) {
+        data = Object.values(snapshot.val());
+        console.log(data);
+    });
+}
+setInterval(fetchValue, 4000);
+
 const toggleButton = document.getElementById('toggle-btn')
 const sidebar = document.getElementById('sidebar')
 var targetdoc = document.querySelector('.value-up');
@@ -30,7 +63,7 @@ function temp() {
     }
 }
 
-setGaugeValue(gaugeElement, (Math.floor(Math.random() * 100))/100);
+setGaugeValue(gaugeElement, data[4]/100);
 }
 
 function humid() {
@@ -57,7 +90,7 @@ function humid() {
 
   }
   
-  setGaugeValue(gaugeElement, (Math.floor(Math.random() * 100))/100);
+  setGaugeValue(gaugeElement, data[1]/100);
 }
 
 function soilM() {
@@ -83,7 +116,7 @@ function soilM() {
     }
   }
   
-  setGaugeValue(gaugeElement, (Math.floor(Math.random() * 100))/100);
+  setGaugeValue(gaugeElement, data[3]/100);
 }
 
 function ltl() {
@@ -94,8 +127,6 @@ function ltl() {
       return;
     }
     
-    console.log(value);
-
     gauge.querySelector(".ltlFill").style.transform = `rotate(${
       value / 2
     }turn)`;
@@ -111,14 +142,14 @@ function ltl() {
     }
 
   }
-  
-  setGaugeValue(gaugeElement, (Math.floor(Math.random() * 100))/100);
+  setGaugeValue(gaugeElement, data[2]/100);
 }
 
-setInterval(temp, 2000);
-setInterval(humid, 2000);
-setInterval(soilM, 2000);
-setInterval(ltl, 2000);
+setInterval(fetchValue, 5000);
+setInterval(temp, 5000);
+setInterval(humid, 5000);
+setInterval(soilM, 5000);
+setInterval(ltl, 5000);
 
 
 function toggleSidebar(){
